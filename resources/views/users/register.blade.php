de066dbf797c3a9c175195156ed443b95e071420
จักรกฤษณ์ <minorin7894@gmail.com>
2023-09-16 15:04:16 +0700

    no message

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -175,6 +176,24 @@
                     </div>
                 </div>
         </div>
+        @if(Session::has('alert'))
+            <script>
+                swal("{{Session::get('alert')}}",{
+                    icon: "success",
+                    if(exist){
+                        alert(msg);
+                }});
+            </script>
+        @endif  
+        @if(Session::has('error'))
+            <script>
+                swal("{{Session::get('error')}}",{
+                    icon: "error",
+                    if(exist){
+                        alert(msg);
+                }});
+            </script>
+        @endif
     </div>
 </body>
 </html>
